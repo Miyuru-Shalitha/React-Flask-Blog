30bd485a7395fbd29840bedcac6b1a8f93a56bcd
Miyuru <miyurushalitha@gmail.com>
2021-10-31 13:20:28 +0530

    Setup alternative db to sqlite

--- server.py
+++ server.py
@@ -8,11 +8,10 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, cur
 
 
 app = Flask(__name__)
-# app.config["SECRET_KEY"] = "It's a secret!"
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///react-flask-blog.db"
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:///react-flask-blog.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
